a25ac5c06fedf36ec9cdc85680a83428056ac69d
Chen Kuang <gnosis-agora@hotmail.com>
2018-06-23 17:29:08 +0800

    Add message handler

---
+++ message_handler.js
@@ -0,0 +1,14 @@
+import { sendMessage } from "./helpers.js";
+
+var processMessage = (event) => {
+	var message = event.message;
+	var senderId = event.sender.id;
+
+	if (message.text) {
+		let prompts = ["hello","hi","yo","what up","hey","hey there","get started"];
+	    let potentialStart = message.text.toLowerCase().replace(/[?.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	    if (prompts.indexOf(potentialStart) != -1) {
+	    	sendMessage(senderId,[{text: "Hello"}])
+	    }
+	}
+}
